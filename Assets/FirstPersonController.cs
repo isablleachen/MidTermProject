6c729d57b8461ffdf5617f586df0cc86e8455af4
IsabellaChen <xc1338@nyu.edu>
2020-02-23 21:48:40 -0500

    Initial commit

---
+++ Assets/FirstPersonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public Rigidbody playerrigidbody;
+    public float backforth;
+    public float leftright;
+
+    public float moveSpeed = 2.5f;
+    public float mouseX, mouseY;
+
+    public Vector3 inputVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerrigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, mouseX, 0);
+        Camera.main.transform.Rotate(-mouseY, 0, 0);
+
+        backforth = Input.GetAxis("Horizontal");
+        leftright = Input.GetAxis("Vertical");
+
+        inputVector = transform.forward * backforth;
+        inputVector -= transform.right * leftright;
+    }
+
+    void FixedUpdate()
+    {
+        playerrigidbody.velocity = (inputVector * moveSpeed * Time.fixedDeltaTime * 50);
+    }
+}
