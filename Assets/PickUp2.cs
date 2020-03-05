d1eb204f34b1483a833dbd253b90846f355ae326
IsabellaChen <xc1338@nyu.edu>
2020-03-05 11:04:43 -0500

    Added more world objects & decoration

---
+++ Assets/PickUp2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp2 : MonoBehaviour
+{
+
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+
+    void Start()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnMouseDown()
+    {
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = tempParent.transform;
+    }
+
+    void OnMouseUp()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.position = guide.transform.position;
+        item.transform.parent = null;
+    }
+}
